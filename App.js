a888246ebf48e2b82ca8584b2e9a5f0166b3862d
Cuong Nguyen <npcuon9@gmail.com>
2021-05-06 11:39:39 +0700

    Login with Facebook

--- App.js
+++ App.js
@@ -1,12 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import * as Facebook from "expo-facebook";
 
 export default function App() {
-  return (
+  const [isLoggedin, setLoggedinStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [isImageLoading, setImageLoadStatus] = useState(false);
+
+  facebookLogIn = async () => {
+    try {
+      await Facebook.initializeAsync({
+        appId: "897285013968583",
+      });
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile"],
+      });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        fetch(
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,picture.height(500)`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setLoggedinStatus(true);
+            setUserData(data);
+          })
+          .catch((e) => console.log(e));
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
+
+  logout = () => {
+    setLoggedinStatus(false);
+    setUserData(null);
+    setImageLoadStatus(false);
+  };
+
+  return isLoggedin ? (
+    userData ? (
+      <View style={styles.container}>
+        <Image
+          style={{ width: 200, height: 200, borderRadius: 50 }}
+          source={{ uri: userData.picture.data.url }}
+          onLoadEnd={() => setImageLoadStatus(true)}
+        />
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          animating={!isImageLoading}
+          style={{ position: "absolute" }}
+        />
+        <Text style={{ fontSize: 22, marginVertical: 10 }}>
+          Hi {userData.name}!
+        </Text>
+        <TouchableOpacity style={styles.logoutBtn} onPress={this.logout}>
+          <Text style={{ color: "#fff" }}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    ) : null
+  ) : (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 50,
+          marginVertical: 20,
+        }}
+        source={require("./assets/favicon.png")}
+      />
+      <TouchableOpacity style={styles.loginBtn} onPress={this.facebookLogIn}>
+        <Text style={{ color: "#fff" }}>Login with Facebook</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +90,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e9ebee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginBtn: {
+    backgroundColor: "#4267b2",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  logoutBtn: {
+    backgroundColor: "grey",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    position: "absolute",
+    bottom: 0,
   },
 });
